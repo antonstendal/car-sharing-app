41514b69936ea26adfcb598bda27106b5e3c0992
hero <antonstendal19@gmail.com>
2026-07-21 13:10:26 +0200

    Add Checkstyle configuration and CI pipeline for build and tests

--- src/main/java/com/example/carsharing/CarSharingApplication.java
+++ src/main/java/com/example/carsharing/CarSharingApplication.java
@@ -1,4 +1,4 @@
-package com.example.car_sharing;
+package com.example.carsharing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CarSharingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CarSharingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CarSharingApplication.class, args);
+    }
 
 }
--- src/test/java/com/example/carsharing/CarSharingApplicationTests.java
+++ src/test/java/com/example/carsharing/CarSharingApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.car_sharing;
+package com.example.carsharing;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
