9afc2402120052966c04f61584db5c347d54a2cc
hero <antonstendal19@gmail.com>
2026-07-21 12:49:53 +0200

    Add project

---
+++ src/test/java/com/example/car_sharing/CarSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.car_sharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSharingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
